3585639b8763c456af76f8d5af648df2f47f22c3
alexnnygaard <143131337+alexnnygaard@users.noreply.github.com>
2024-04-25 15:42:26 +0200

    Fjerna kode som ikke brukes

--- src/main/java/oblig3data1700java/BillettController.java
+++ src/main/java/oblig3data1700java/BillettController.java
@@ -23,9 +23,6 @@ public class BillettController {
         System.out.println(billetter.getEpost());
         System.out.println(billetter.getTelefon());
 
-
-
-
         rep.lagreBillett(billetter);
     }
 
@@ -39,47 +36,4 @@ public class BillettController {
         rep.slettAlle();
     }
 }
-
-/*import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
-
-@RestController
-public class BillettController {
-
-    @Autowired
-    BillettRepository rep;
-
-    @PostMapping("/lagre")
-    public void lagreBillett(Billett billetter, HttpServletResponse res) throws IOException {
-        boolean ok = rep.lagreBillett(billetter);
-        if (!ok) {
-            res.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "En feil har oppstått, forsøk igjen senere");
-        }
-    }
-
-    @GetMapping("/hentAlle")
-    public List<Billett> hentAlle(HttpServletResponse res) throws IOException {
-        List<Billett> billettene = rep.hentAlle();
-        if (billettene == null) {
-            res.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "En feil har oppstått, forsøk igjen senere");
-        }
-        return billettene;
-    }
-    @GetMapping("/slettAlle")
-    public void slettAlle(HttpServletResponse res) throws IOException {
-        boolean ok = rep.slettAlle();
-        if (!ok){
-            res.sendError(HttpStatus.INTERNAL_SERVER_ERROR.value(),"Database-feil, forsøk igjen senere");
-        }
-    }
-}
-
-*/
 
